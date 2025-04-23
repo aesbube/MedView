47fb3a59b55a98d6b13ca1659cfbe9a250e19dfa
Marko Durakov <marko.durakov@sorsix.com>
2025-04-23 14:04:02 +0200

    Added home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SearchesComponent } from "../searches/searches.component";
+import { MapComponent } from "../map/map.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [SearchesComponent, MapComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
