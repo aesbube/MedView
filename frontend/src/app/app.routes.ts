6ba50bae38a017d6395f5d449aa1e48abbc29a84
bube <ljubicad11@gmail.com>
2025-04-29 11:28:00 +0200

    Added redirection from login if a user is already logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PatientProfileComponent } from './components/patient-profile/patient-profile.component';
+import { AuthRedirectGuard } from './guards/auth-redirect.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [AuthRedirectGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: PatientProfileComponent },
 ];
